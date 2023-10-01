aa9b2cacaa1730914370564f3ebd8c2f22bb4097
omkute <58985673+omkute@users.noreply.github.com>
2023-10-01 15:13:09 +0530

    push 2

---
+++ app/components/Nav.js
@@ -0,0 +1,12 @@
+import { View,Text } from "react-native"
+
+
+const Nav = ()=>{
+    return(
+        <View>
+            <Text>Nav</Text>
+        </View>
+    )
+}
+
+export default Nav;
--- app/index.js
+++ app/index.js
@@ -1,12 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
+import Nav from "./components/Nav";
+
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Hello World</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
+      <Nav/>
     </View>
   );
 }
